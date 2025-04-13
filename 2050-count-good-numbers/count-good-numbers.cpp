af8a8e7d6ad7054a2cd07f5e926c396249e801b7
Somesh Shukla <someshshuklacollege@gmail.com>
2025-04-14 02:41:35 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (3.20%) - LeetSync

---
+++ 2050-count-good-numbers/count-good-numbers.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    #define ll long long
+    int MOD=1000000007;
+    ll binaryexpo(ll a,ll b){
+        ll res =1;
+        while(b){
+            if(b&1)
+            res=(res*a)%MOD;
+            a=(a*a)%MOD;
+            b/=2;
+        }
+        return res;
+    }
+    int countGoodNumbers(long long n) {
+    return (binaryexpo(4,n/2)*binaryexpo(5,n-n/2))%MOD;    
+    }
+};
