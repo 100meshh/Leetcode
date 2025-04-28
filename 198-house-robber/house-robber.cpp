d906832256eb62e92941557443fb0906f3b4bb56
Somesh Shukla <someshshuklacollege@gmail.com>
2025-04-28 13:33:36 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (89.74%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,16 +1,28 @@
 class Solution {
 public:
-int f(int ind,vector<int>&nums,vector<int>&dp){
-    if(ind==0) return nums[0];
-    if(ind<0) return 0;
-    if(dp[ind]!=-1) return dp[ind];
-    int take=nums[ind]+f(ind-2,nums,dp);
-    int notTake=0+f(ind-1,nums,dp);
-    return dp[ind]=max(take,notTake);
-}
+// int f(int ind,vector<int>&nums,vector<int>&dp){
+//     dp[0]=0;
+//     int neg=0;
+    // if(ind==0) return nums[0];
+    // if(ind<0) return 0;
+    // for(int i=1;i<n;i++){
+    // int take=nums[ind]; if(ind>1) take+=dp[ind-2];
+    // int notTake=0+dp[ind-1];
+    // dp[ind]=max(take,notTake);
+    // }
+// }
     int rob(vector<int>& nums) {
-       int n=nums.size();
-       vector<int>dp(n,-1);
-       return f(n-1,nums,dp); 
+        int n=nums.size();
+        int prev=nums[0];
+        int prev2=0;
+        for(int ind=1;ind<n;ind++){
+        int take=nums[ind];
+        if(ind>1) take+=prev2;
+        int notTake=0+prev;
+        int curI=max(take,notTake);
+        prev2=prev;
+        prev=curI;
+        }
+        return prev;
     }
 };
