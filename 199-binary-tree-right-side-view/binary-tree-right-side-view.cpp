493504257f9bf9539ededcae865af99cda64afb9
Somesh Shukla <someshshuklacollege@gmail.com>
2025-05-12 01:06:54 +0530

    Time: 0 ms (100.00%) | Memory: 14.8 MB (95.49%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -9,28 +9,17 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
 class Solution {
-  public:
+public:
+void rview(TreeNode* root , int level,vector<int>&ans){
+    if(!root) return;
+    if(level==ans.size()) ans.push_back(root->val);
+    rview(root->right,level+1,ans);
+    rview(root->left,level+1,ans);
+}
     vector<int> rightSideView(TreeNode* root) {
-        // code here
         vector<int>ans;
-        if(!root) return ans;
-        queue<TreeNode*>q;
-        q.push(root);
-        while(!q.empty()){
-            int n=q.size();
-            ans.push_back(q.front()->val);
-            
-            while(n--)
-            {
-                TreeNode* temp;
-                temp=q.front();
-                q.pop();
-                if(temp->right)q.push(temp->right);
-                if(temp->left)q.push(temp->left);
-            }
-        }
+        rview(root,0,ans);
         return ans;
     }
 };
